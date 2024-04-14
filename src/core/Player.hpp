14549af7c62698c2baab588c7934fa34ab33deeb
ji2chang <166942946+ji2chang@users.noreply.github.com>
2024-04-14 14:59:46 +0200

    Update Player.hpp
    movimento del Player

--- src/core/Player.hpp
+++ src/core/Player.hpp
@@ -1,18 +1,19 @@
 #include "raylib.h"
+#include "World.hpp"
 namespace raycaster {
     class Player {
         Vector2 position;
         double direction;
-
+        World gameMap;
     public:
-        Player(const Vector2 &position, double direction);
-
-        Player();
+        Player(const Vector2 &position, double direction, const World &gameMap);
 
         const Vector2 &getPosition() const;
 
         double getDirection() const;
 
         void rotate(float angle);
+
+        void move(Vector2 displacement);
     };
 }
