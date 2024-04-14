544630a5f6cd26c42e854c6b3b0197d18387c0c7
ji2chang <166942946+ji2chang@users.noreply.github.com>
2024-04-14 15:01:23 +0200

    Update main.cpp
    movimento del Player

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ int main(void)
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
+    const float step = 0.1;
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -17,7 +18,7 @@ int main(void)
     auto screenCentre = GetWindowPosition();
     auto renderer = raycaster::Renderer(800);
     auto world = raycaster::World("../res/maps/base.map");
-    auto player = raycaster::Player(Vector2{5,5},0);
+    auto player = raycaster::Player(Vector2{5,5},0,world);
     HideCursor();
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -26,7 +27,21 @@ int main(void)
         // Update
         //----------------------------------------------------------------------------------
         if(IsWindowFocused()) {
-            player.rotate(raycaster::deg2rad(screenCentre.x - GetMouseX()) / 2);
+            player.rotate((float )raycaster::deg2rad((float)GetMouseX()-screenCentre.x) / 2);
+            if(IsKeyDown(KEY_W)){
+                player.move({step,0});
+
+            }
+            if(IsKeyDown(KEY_A)){
+                player.move({0,-step});
+
+            }
+            if(IsKeyDown(KEY_S)){
+                player.move({-step,0});
+            }
+            if(IsKeyDown(KEY_D)){
+                player.move({0,step});
+            }
         }
         renderer.CalculateWallVec(world, player.getPosition(), player.getDirection());
         SetMousePosition((int)screenCentre.x,(int)screenCentre.y);
